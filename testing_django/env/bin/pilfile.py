64e6cb1f2b9fb36bf67bace22c14a7ac51da4823
root <root@python-gaming.com>
2015-10-28 00:06:54 -0400

    cleaned filesystem

--- testing_django/env/bin/pilfile.py
+++
@@ -1,5 +0,0 @@
-#!/var/www/html/testing_django/env/bin/python
-# EASY-INSTALL-SCRIPT: 'Pillow==3.0.0','pilfile.py'
-__requires__ = 'Pillow==3.0.0'
-import pkg_resources
-pkg_resources.run_script('Pillow==3.0.0', 'pilfile.py')
